0f157bace4df1e347229df39b7ea13ed570cc99d
uraqta <125738383+uraqta@users.noreply.github.com>
2023-03-17 22:23:01 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+var num  = document.querySelectorAll(".drum").length;
+for(i = 0; i<num; i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+
+        
+        var buttoninnerhtml = this.innerHTML;
+        sound(buttoninnerhtml);
+      buttonanimation(buttoninnerhtml);
+  });
+  }
+  
+
+document.addEventListener("keypress", function(event){
+  sound(event.key);
+  buttonanimation(event.key);
+})
+ 
+
+function sound(key){
+  switch(key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3")
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3")
+      tom2.play();
+      break;
+    case "s" :
+      var tom3 = new Audio("sounds/tom-3.mp3")
+      tom3.play();
+     case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3")
+      tom4.play();
+    case "j":
+      var snare = new Audio("sounds/snare.mp3")
+      snare.play();
+    case "k":
+      var kick = new Audio("sounds/crash.mp3")
+      kick.play();
+    case "l":
+      var crash  = new Audio("sounds/kick-bass.mp3")
+      crash.play();
+
+
+    default:
+      // code block
+  }
+}
+function buttonanimation(currentkey){
+  var hones = document.querySelector("."+currentkey);
+ 
+ hones.classList.add("pressed");
+
+setTimeout(function(){
+  hones.classList.remove("pressed");
+},100)
+}
