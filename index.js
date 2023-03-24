e835a38dd12d18075f106764a8a4cba09b708aff
uraqta <125738383+uraqta@users.noreply.github.com>
2023-03-24 21:30:08 +0545

    Update index.js

--- index.js
+++ index.js
@@ -29,19 +29,23 @@ function sound(key){
     case "s" :
       var tom3 = new Audio("sounds/tom-3.mp3")
       tom3.play();
+      break;
      case "d":
       var tom4 = new Audio("sounds/tom-4.mp3")
       tom4.play();
+      break;
     case "j":
       var snare = new Audio("sounds/snare.mp3")
       snare.play();
+       break;
     case "k":
       var kick = new Audio("sounds/crash.mp3")
       kick.play();
+       break;
     case "l":
       var crash  = new Audio("sounds/kick-bass.mp3")
       crash.play();
-
+   break;
 
     default:
       // code block
